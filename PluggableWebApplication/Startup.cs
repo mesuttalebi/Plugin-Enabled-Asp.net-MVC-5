a7324556b94c820e681c5414a38d5517034f850c
Mesut Talebi <mesut.talebi@yahoo.com>
2015-02-06 16:47:43 +0200

    Initial Commit

---
+++ PluggableWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluggableWebApplication.Startup))]
+namespace PluggableWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
